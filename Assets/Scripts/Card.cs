1dcd8fe80dc8a04318948a3f3ea8b73d0dc44abb
zkco <qkrentks456@gmail.com>
2024-09-09 14:37:30 +0900

    [Def] role

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public GameObject card;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
