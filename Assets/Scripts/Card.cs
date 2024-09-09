d52fa50d3a899fb8a719ff8d67c2cd14c5d6a877
zkco <qkrentks456@gmail.com>
2024-09-09 19:43:41 +0900

    [Fix] CardFlip anims
    Scale -> Y Rotation

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -21,6 +21,8 @@ public class Card : MonoBehaviour
         anim.SetBool("isOpen", true);
         front.SetActive(true);
         back.SetActive(false);
+
+        
     }
 
     public void DestroyCard()
@@ -31,5 +33,17 @@ public class Card : MonoBehaviour
     public void InvokeDestroyCard()
     {
         Invoke("DestroyCard", 1.0f);
+    }
+
+    public void CloseCard() //카드가 틀리다면 다시 뒤집는 함수
+    {
+        Invoke("CloseCardInvoke", 1.0f);
+    }
+
+    public void CloseCardInvoke()
+    {
+        anim.SetBool("isOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
     }
 }
