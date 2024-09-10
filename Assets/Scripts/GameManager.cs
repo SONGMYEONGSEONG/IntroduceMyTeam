7f1287b828b786eb99324ba35b9513ebb8cce92b
zkco <qkrentks456@gmail.com>
2024-09-10 14:26:26 +0900

    [update] GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public Card firstCard;
+    public Card secondCard;
 
     private void Awake()
     {
@@ -12,11 +14,32 @@ public class GameManager : MonoBehaviour
             Instance = this;
     }
 
-    public GameObject firstCard;
-    public GameObject secondCard;
+    private void Start()
+    {
+    }
+
+
 
     private void Update()
     {
+        
+    }
 
+    public void Matched() //카드가 일치하는지 확인 하는 함수
+    {
+        if (firstCard.idx == secondCard.idx) //카드가 같다면
+        {
+            firstCard.DestroyCard();
+            secondCard.DestroyCard();
+        }
+        else //카드가 틀리다면
+        {
+            firstCard.CloseCard();
+            secondCard.CloseCard();
+        }
+
+        firstCard = null; //오브젝트 값 초기화
+        secondCard = null;
     }
 }
+
