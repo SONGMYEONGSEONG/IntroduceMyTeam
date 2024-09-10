66d2940da5a003cbd2e7712a74d8bfa25e33ea54
GammaTT <52464999+GammaTT@users.noreply.github.com>
2024-09-10 12:05:19 +0900

    보드 배열 수정

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -15,7 +15,7 @@ public class Board : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+        int[] arr = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8 };
         arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
 
         for (int i = 0; i < 4; i++)
@@ -23,7 +23,12 @@ public class Board : MonoBehaviour
             for (int j = 0; j < 4; j++)
             {
                 GameObject gameObject = Instantiate(card);
-                gameObject.GetComponent<Card>().SetImage(arr[i]);
+
+                int temp = arr[0];
+                arr = arr.Skip(1).ToArray();
+
+                gameObject.GetComponent<Card>().SetImage(temp);
+
                 Vector2 pos = new Vector2(j * 1.4f + xAdjustment, i * 1.4f + yAdjustment);
                 gameObject.transform.position = pos;
             }
