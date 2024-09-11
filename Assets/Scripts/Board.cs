282d58adf649deed2ca7af90c2fd8dcd8ae37a7b
SONGMYEONGSEONG <100818983+SONGMYEONGSEONG@users.noreply.github.com>
2024-09-11 16:05:28 +0900

    [Fix]StageSelectScene
    [fix]

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.Burst.Intrinsics;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 public class Board : MonoBehaviour
 {
