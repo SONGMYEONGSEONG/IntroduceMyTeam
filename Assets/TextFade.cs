4d8cab9d85e4861a4f0c5bcb07dbd566411e4e45
SONGMYEONGSEONG <100818983+SONGMYEONGSEONG@users.noreply.github.com>
2024-09-12 10:59:16 +0900

    [Add]BonusTime

--- Assets/TextFade.cs
+++ Assets/TextFade.cs
@@ -6,17 +6,6 @@ using UnityEngine.UI;
 public class TextFade : MonoBehaviour
 {
     public Text text;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void StartFadeToZero()
     {
